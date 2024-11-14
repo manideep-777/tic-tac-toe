07a7e9dc773efaf5488d815166bdd446dddf8367
mani <manideepknv@gmail.com>
2024-11-14 14:34:23 +0530

    formated code

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,7 +1,7 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import Sqaure from './Sqaure'
 import './App.css'
-import { check_row, check_cols, check_cross_dig1, check_cross_dig2 ,check_tie } from './boardUtils'
+import { check_row, check_cols, check_cross_dig1, check_cross_dig2, check_tie } from './boardUtils'
 
 export default function Board() {
 
@@ -23,7 +23,7 @@ export default function Board() {
             await check_cross_dig2(boardArr, xscore, oscore, setWin, setXscore, setOscore, setCount);
         }
         func();
-    },[boardArr])
+    }, [boardArr])
 
     useEffect(() => {
         if (win === "" && boardArr.length === 9) {
@@ -35,12 +35,12 @@ export default function Board() {
     }, [win, boardArr]);
 
     useEffect(() => {
-        if(win!=""){
+        if (win != "") {
             setStopGame(true);
-            if(win == "X"){
+            if (win == "X") {
                 setxo("O");
             }
-            if(win == "O"){
+            if (win == "O") {
                 setxo("X");
             }
         }
@@ -55,123 +55,123 @@ export default function Board() {
     }, [newGame]);
 
     useEffect(() => {
-      
-    console.log(whoStart);
-      
-    }, [whoStart])
-    
-    
-    
-    
 
-    function handleClick(index){
+        console.log(whoStart);
+
+    }, [whoStart])
+
+
+
+
+
+    function handleClick(index) {
         let play = "";
-        if(!stopGame && boardArr[index-1] == null){
-            let i = parseInt(index)-1
-            if(count%2 == 0){
+        if (!stopGame && boardArr[index - 1] == null) {
+            let i = parseInt(index) - 1
+            if (count % 2 == 0) {
                 play = "O"
                 setBoardArr(
-                    boardArr.map((el,index)=>{
-                        if(index==i){
+                    boardArr.map((el, index) => {
+                        if (index == i) {
                             return "O"
                         }
                         return el
                     })
                 )
             }
-            if(count%2 != 0){
+            if (count % 2 != 0) {
                 play = "X"
                 setBoardArr(
-                    boardArr.map((el,index)=>{
-                        if(index==i){
+                    boardArr.map((el, index) => {
+                        if (index == i) {
                             return "X"
                         }
                         return el
                     })
                 )
             }
-            setCount(count+1);
+            setCount(count + 1);
             setxo(play)
             return play
         }
-        return boardArr[index-1]
+        return boardArr[index - 1]
     }
 
-    
+
 
     function nextGame() {
         setBoardArr(Array(9).fill(null));
         setStopGame(false);
         setNewGame(!newGame);
 
-        if(win == "X"){
+        if (win == "X") {
             setWhoStart("X");
             setxo("O");
             setCount(1);
         }
-        if(win == "O"){
+        if (win == "O") {
             setWhoStart("O");
             setxo("X");
             setCount(0);
         }
-        
+
         setWin("");
     }
 
-    function resetGame(){
+    function resetGame() {
         setBoardArr(Array(9).fill(null));
         setStopGame(false);
         setNewGame(!newGame);
-        if(whoStart == "X"){
+        if (whoStart == "X") {
             setCount(1);
             setxo("O")
         }
-        if(whoStart == "O"){
+        if (whoStart == "O") {
             setCount(0);
             setxo("X");
         }
         setWin("");
     }
 
-  return (
-    <div className={xo == "O" ? "bg-red-400" : "bg-blue-400"}>   
-      <div className='flex flex-col justify-center items-center h-screen w-screen' >
-        <div className={`text-2xl text-white font-semibold`}> {whoStart} Starts First </div>
-        <div className="flex m-5">
-            <p className={`text-xl text-white font-medium`}>X - Score : {xscore}</p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <p className={`text-xl text-white font-medium`}>O - Score : {oscore}</p>
-        </div>
-        <div className="line1 flex">
-            <Sqaure index="1" click={handleClick} newGame={newGame} classes={"border-l-0 border-t-0 border-white"}/>
-            <Sqaure index="2" click={handleClick} newGame={newGame} classes={"border-t-0 border-white"}/>
-            <Sqaure index="3" click={handleClick} newGame={newGame} classes={"border-r-0 border-t-0 border-white"}/>
-        </div>
-        <div className="line2 flex">
-            <Sqaure index="4" click={handleClick} newGame={newGame} classes={"border-l-0 border-white"}/>
-            <Sqaure index="5" click={handleClick} newGame={newGame} classes={" border-white"}/>
-            <Sqaure index="6" click={handleClick} newGame={newGame} classes={"border-r-0 border-white"}/>
-        </div>
-        <div className="line3 flex">
-            <Sqaure index="7" click={handleClick} newGame={newGame} classes={"border-l-0 border-b-0 border-white"}/>
-            <Sqaure index="8" click={handleClick} newGame={newGame} classes={"border-b-0 border-white"}/>
-            <Sqaure index="9" click={handleClick} newGame={newGame} classes={"border-r-0 border-b-0 border-white"}/>
-        </div>
-        <div className='m-5 text-white text-xl font-semibold'>{win=="it's a tie" ? win : win != "" ? win+" wins" : ""}</div>
-        <div className="flex">
-            <div className="mx-2">
-                <button type="button"  className="bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={nextGame}>
-                    Start New Game
-                </button>
+    return (
+        <div className={xo == "O" ? "bg-red-400" : "bg-blue-400"}>
+            <div className='flex flex-col justify-center items-center h-screen w-screen' >
+                <div className={`text-2xl text-white font-semibold`}> {whoStart} Starts First </div>
+                <div className="flex m-5">
+                    <p className={`text-xl text-white font-medium`}>X - Score : {xscore}</p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p className={`text-xl text-white font-medium`}>O - Score : {oscore}</p>
+                </div>
+                <div className="line1 flex">
+                    <Sqaure index="1" click={handleClick} newGame={newGame} classes={"border-l-0 border-t-0 border-white"} />
+                    <Sqaure index="2" click={handleClick} newGame={newGame} classes={"border-t-0 border-white"} />
+                    <Sqaure index="3" click={handleClick} newGame={newGame} classes={"border-r-0 border-t-0 border-white"} />
+                </div>
+                <div className="line2 flex">
+                    <Sqaure index="4" click={handleClick} newGame={newGame} classes={"border-l-0 border-white"} />
+                    <Sqaure index="5" click={handleClick} newGame={newGame} classes={" border-white"} />
+                    <Sqaure index="6" click={handleClick} newGame={newGame} classes={"border-r-0 border-white"} />
+                </div>
+                <div className="line3 flex">
+                    <Sqaure index="7" click={handleClick} newGame={newGame} classes={"border-l-0 border-b-0 border-white"} />
+                    <Sqaure index="8" click={handleClick} newGame={newGame} classes={"border-b-0 border-white"} />
+                    <Sqaure index="9" click={handleClick} newGame={newGame} classes={"border-r-0 border-b-0 border-white"} />
+                </div>
+                <div className='m-5 text-white text-xl font-semibold'>{win == "it's a tie" ? win : win != "" ? win + " wins" : ""}</div>
+                <div className="flex">
+                    <div className="mx-2">
+                        <button type="button" className="bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={nextGame}>
+                            Start New Game
+                        </button>
+                    </div>
+                    <div className="mx-2">
+                        <button type="button" className="bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={resetGame}>
+                            Reset Existing Game
+                        </button>
+                    </div>
+
+                </div>
+
             </div>
-            <div className="mx-2">
-                <button type="button"  className="bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={resetGame}>
-                    Reset Existing Game
-                </button>
-            </div>
-            
         </div>
-        
-      </div>
-    </div>
-  )
+    )
 }
